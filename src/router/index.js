1f8750444a0005451b9f9252d17eec2beb964f4e
Sethxy123 <sethneftali12@gmail.com>
2026-01-13 16:58:19 -0600

    hola

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
 import HomeView from '../views/HomeView.vue'
-import RegisterView from '../views/RegisterView.vue' 
-import { createRouter, createWebHashHistory } from 'vue-router'// <--- 1. IMPORTAR
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHashHistory(),
+  // Usamos HashHistory para que funcione en GitHub Pages
+  history: createWebHashHistory(), 
   routes: [
-    { path: '/login', name: 'login', component: LoginView },
-    { path: '/register', name: 'register', component: RegisterView }, // <--- 2. AGREGAR ESTA LÍNEA
-    { 
-      path: '/', 
-      name: 'home', 
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/',
+      name: 'home',
       component: HomeView,
       beforeEnter: (to, from, next) => {
-        if (!localStorage.getItem('token')) next('/login');
-        else next();
+        if (!localStorage.getItem('token')) {
+          next('/login');
+        } else {
+          next();
+        }
       }
     }
   ]
